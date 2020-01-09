53b64534251969ff71dec4f2bef0daf381dbfa08
wangqiang <1095695450@qq.com>
2020-01-10 01:15:52 +0800

    add test files for objdump

---
+++ trash/test20.c
@@ -0,0 +1,16 @@
+int a = 4;
+
+int func()
+{
+    int b = a;
+    int c = 0;
+    if (b < 5)
+    {
+        c = 1;
+    }
+    else
+    {
+        c = 2;
+    }
+    return 0;
+}
---
+++ trash/wangtest/src/main.c
@@ -0,0 +1,14 @@
+int a = 1;
+int b = 100;
+int c = 288;
+
+int main()
+{
+    int local_a;
+
+    local_a = c;
+
+    if (local_a < 500)
+        return 0;
+    return 0;
+}
